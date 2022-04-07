d35da7123414a0ee74f152ffaff1724fa23f297e
oceansalle <oceanesalleiae@gmail.com>
2022-04-07 16:42:07 +0200

    about section

--- portfolio-1/src/components/Home.jsx
+++ portfolio-1/src/components/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Home.css'; 
 
-
 function Home() {
   return (
     <div classname="Home">
@@ -13,19 +12,28 @@ function Home() {
           <div className="navigation">
             <ul className="navbar d__flex">
               <a href="#Home"><li className="nav__items mx__15">Home</li></a>
-              <a href="#Home"><li className="nav__items mx__15">Home</li></a>
-              <a href="#Home"><li className="nav__items mx__15">Home</li></a>
-              <a href="#Home"><li className="nav__items mx__15">Home</li></a>
-              <a href="#Home"><li className="nav__items mx__15">Home</li></a>
+              <a href="#About"><li className="nav__items mx__15">About</li></a>
+              <a href="#Services"><li className="nav__items mx__15">Services</li></a>
+              <a href="#Portfolio"><li className="nav__items mx__15">Portfolio</li></a>
+              <a href="#Blog"><li className="nav__items mx__15">Blog</li></a>
+              <a href="#Contact"><li className="nav__items mx__15">Contact</li></a>
             </ul>
           </div>
         </div>
+          {/* HOME */}
+          <div className="container">
+          <div className="home__content">
+            <div className="home__meta">
+              <h1 className="home__text pz__10">WELCOME TO MY PORTFOLIO</h1>
+              <h2 className="home__text pz__10">Hi, I’m Océane</h2>
+              <h3 className="home__text sweet pz__10">Fullstack developer.</h3>
+            </div>
+          </div>
+        </div>
       </div>
-
-
     </div>
   )
 }
 
 
-export default Home
+export default Home; 
