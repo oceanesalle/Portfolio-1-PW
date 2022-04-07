eb3467a8b7cadceed57e16de5cf042671cd4fd3b
oceansalle <oceanesalleiae@gmail.com>
2022-04-07 18:40:20 +0200

    services section

--- portfolio-1/src/App.js
+++ portfolio-1/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Home from "./Home";
 import About from "./About" ;
+import Services from "./Services"; 
 
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
+      <Services />
+      
 
       
     </div>
